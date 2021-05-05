b739981d89f9efcef738724500b90d1bfb921833
Rajatsharma-07 <65624164+Rajatsharma-07@users.noreply.github.com>
2021-05-05 12:31:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//Your age in days
+
+function ageInDays() {
+    var birthYear = prompt("What year you were born?");
+    var ageInDayss = (2021 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageInDayss + ' old.');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
